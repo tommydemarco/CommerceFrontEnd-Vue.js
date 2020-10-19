e083e4f56750af5c3cb9a8d7562fb9beba1bc8b4
tommydemarco <tommaso.demarco95@gmail.com>
2020-10-19 22:32:14 +0100

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <the-header></the-header>
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+import TheHeader from '@/components/TheHeader.vue'
+export default {
+  components: {
+    TheHeader
+  }
+  
+}
+</script>
+
+<style>
+
+</style>
